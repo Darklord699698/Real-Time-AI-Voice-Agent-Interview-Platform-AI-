c4ed1ceeaa11a4224a9bd61215718d5d30d08ae7
UJJWAL <ujjwal8851114020@gmail.com>
2025-04-16 01:03:41 +0530

    signin/signup

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -15,12 +15,13 @@ import {
   FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
+import Image from "next/image"
 
 const formSchema = z.object({
   username: z.string().min(2).max(50),
 })
 
-const AuthForm = () => {
+const AuthForm = ({ type }:{type:FormType}) => {
 
     // 1. Define your form.
   const form = useForm<z.infer<typeof formSchema>>({
@@ -37,29 +38,29 @@ const AuthForm = () => {
     console.log(values)
   }
 
+  const isSignIn = type === "sign-in";
+
   return (
-    <div>
+    <div className="card-border lg:min-w-[566px]">
+        <div className="flex flex-col gap-6 card py-14 px-10">
+            <div className="flex flex-row gap-2 justify-center">
+                <Image 
+                src="/logo.svg" 
+                alt="logo" 
+                height={32} 
+                width={38} />
+                <h2 className="text-primary-100">PrepWise</h2>
+            
+            <h3>Practice job interviews with AI</h3>
+        </div>
         <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+      <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-6 mt-4 form">
+        {!isSignIn && <p>Name</p>}
+        
         <Button type="submit">Submit</Button>
       </form>
     </Form>
+    </div>
     </div>
   )
 }
